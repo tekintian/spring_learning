ecfb697db9dd8790b6bcabd2c54203ae65ab8092
Tekin Tian <tekintian@gmail.com>
2018-05-28 11:13:22 +0800

    spring 框架，小试牛刀之  HelloWorld

---
+++ src/main/java/cn/tekin/spring/demo1/MainApp.java
@@ -0,0 +1,27 @@
+package cn.tekin.spring.demo1;
+
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.AbstractXmlApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * 演示
+ * @author Tekin
+ */
+public class MainApp {
+    public static void main(String[] args) {
+        //ApplicationContext 方式
+       /* ApplicationContext applicationContext=new ClassPathXmlApplicationContext("Beans.xml");
+        HelloWorld helloWorld= (HelloWorld) applicationContext.getBean("helloWorld");
+        */
+
+       //BeanFactory
+        BeanFactory beanFactory=new ClassPathXmlApplicationContext("Beans.xml");
+        HelloWorld helloWorld=(HelloWorld) beanFactory.getBean("helloWorld");
+
+        System.out.println("message:"+helloWorld.getMessage());
+
+    }
+}
